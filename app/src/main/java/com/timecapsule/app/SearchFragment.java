5b91203d69fd35f3d373a45d7c69f60a75bcc9ec
HakeemSackes-Bramble <hakksaxx@outlook.com>
2017-03-11 17:14:14 -0500

    adding location data

--- app/src/main/java/com/timecapsule/app/SearchFragment.java
+++ app/src/main/java/com/timecapsule/app/SearchFragment.java
@@ -2,8 +2,8 @@ package com.timecapsule.app;
 
 import android.Manifest;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -31,28 +32,12 @@ import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class SearchFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
+    private static final String TAG = SearchFragment.class.getSimpleName();
     private View mRoot;
     private GoogleMap mMap;
-    private Location location;
+    private FusedLocationProviderApi location;
     private GoogleApiClient googleApiClient;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        googleApiClient = new GoogleApiClient
-                .Builder(getApplicationContext())
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addApi(LocationServices.API)
-                .build();
-        googleApiClient.connect();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        googleApiClient.disconnect();
-    }
+    private FusedLocationProviderApi fusedLocationProviderApi;
 
 
     @Override
@@ -62,8 +47,42 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback, Goog
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+        googleApiClient = new GoogleApiClient
+                .Builder(getApplicationContext())
+                .addConnectionCallbacks(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+        fusedLocationProviderApi = LocationServices.FusedLocationApi;
+        if(googleApiClient.isConnected()){
+            Log.d(TAG, "Was connected");
+            fusedLocationProviderApi.getLastLocation(googleApiClient);
+        } else {
+            Log.d(TAG, "Wasn't connected");
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+
 
     @Nullable
     @Override
@@ -90,37 +109,39 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback, Goog
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(this.getClass().getSimpleName(), "onMapReady: ");
 
             return;
         }
         mMap.setMyLocationEnabled(true);
+        Log.d(this.getClass().getSimpleName(), "onMapReady: ");
 
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.d("no location", "onConnectionFailed: do something");
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        Log.d(this.getClass().getSimpleName(), "onConnected: ");
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
-            Log.d("yyyy", "onConnected: shyt");
-             return;
+            Log.d("not allowed", "onConnected: shyt");
+            return;
         }
-        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude()); //40.7128° N, -74.0059° W
 
+        fusedLocationProviderApi = LocationServices.FusedLocationApi;
+        location = fusedLocationProviderApi;
+        LatLng currentLocation = new LatLng(location.getLastLocation(googleApiClient).getLatitude(), location.getLastLocation(googleApiClient).getLongitude()); //40.7128° N, -74.0059° W
+        Log.d("", "onConnected: " + location.getLastLocation(googleApiClient).getLatitude() + " " + location.getLastLocation(googleApiClient).getLongitude());
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
-
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
     }
 
     @Override
     public void onConnectionSuspended(int i) {
+        Log.d(this.getClass().getSimpleName(), "onConnectionSuspended: " + String.valueOf(i));
 
     }
 }
