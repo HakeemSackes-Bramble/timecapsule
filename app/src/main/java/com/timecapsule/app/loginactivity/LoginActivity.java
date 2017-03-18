faa166450b807c094943e272747e6f46e227eae0
catcattac <catwong.nyc@gmail.com>
2017-03-18 01:15:12 -0400

    changed login screen, again

--- app/src/main/java/com/timecapsule/app/loginactivity/LoginActivity.java
+++ app/src/main/java/com/timecapsule/app/loginactivity/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.timecapsule.app.R;
 
 /**
@@ -16,6 +18,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private static final String TAG = "Login Information";
     ImageView iv_sign_in;
     ImageView iv_sign_up;
+    ImageView logo;
 
 
     @Override
@@ -35,16 +38,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void setViews() {
         iv_sign_in = (ImageView) findViewById(R.id.iv_login_signin);
         iv_sign_up = (ImageView) findViewById(R.id.iv_login_signup);
+        logo = (ImageView) findViewById(R.id.iv_login_logo);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(logo);
+        Glide.with(this)
+                .load(R.drawable.gif_time_capsule)
+                .crossFade()
+                .into(imageViewTarget);
     }
 
     private void setSignInClickView() {
-        iv_sign_in.setImageResource(R.drawable.signin_8);
-        iv_sign_up.setImageResource(R.drawable.signup8_uc);
+        iv_sign_in.setImageResource(R.drawable.signin9);
+        iv_sign_up.setImageResource(R.drawable.signup9_uc);
     }
 
     private void setSignUpClickView() {
-        iv_sign_up.setImageResource(R.drawable.signup_8);
-        iv_sign_in.setImageResource(R.drawable.signin8_uc);
+        iv_sign_up.setImageResource(R.drawable.signup9);
+        iv_sign_in.setImageResource(R.drawable.signin9_uc);
     }
 
     private void setSignIn() {
