3f81a2246826d628de43e96e0785c4d8f8899659
Jose Gonzalez <mathcore@gmail.com>
2017-03-20 02:03:02 -0400

    populating user list view

--- app/src/main/java/com/timecapsule/app/users/UserListAdapter.java
+++ app/src/main/java/com/timecapsule/app/users/UserListAdapter.java
@@ -3,9 +3,18 @@ package com.timecapsule.app.users;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.timecapsule.app.R;
 import com.timecapsule.app.profilefragment.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by catwong on 3/18/17.
@@ -14,7 +23,34 @@ import com.timecapsule.app.profilefragment.model.User;
 public class UserListAdapter extends ArrayAdapter<User> {
 
 
-    public UserListAdapter(@NonNull Context context, @LayoutRes int resource) {
-        super(context, resource);
+    public UserListAdapter(Context context, List<User> users) {
+        super(context, R.layout.user_list_single, users);
     }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        UsersFragment.ViewHolder holder;
+
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.user_list_single, null);
+            holder = new UsersFragment.ViewHolder();
+            holder.tvUserName = (TextView) convertView.findViewById(R.id.tv_name);
+            holder.tvEmail = (TextView) convertView.findViewById(R.id.tv_email);
+            convertView.setTag(holder);
+
+        }else {
+            holder = (UsersFragment.ViewHolder) convertView.getTag();
+        }
+
+        User user = getItem(position);
+        holder.tvUserName.setText(user.getName());
+        holder.tvEmail.setText(user.getEmail());
+
+        return convertView;
+    }
+
+
 }
