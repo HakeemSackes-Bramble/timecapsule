b10a650163d1521c76b705f76f4f3c1304763f72
catcattac <catwong.nyc@gmail.com>
2017-03-20 21:18:49 -0400

    fixed layouts for hub

--- app/src/main/java/com/timecapsule/app/searchfragment/TimeCapsuleHubFragment.java
+++ app/src/main/java/com/timecapsule/app/searchfragment/TimeCapsuleHubFragment.java
@@ -1,14 +1,17 @@
 package com.timecapsule.app.searchfragment;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.timecapsule.app.R;
 import com.timecapsule.app.profilefragment.model.Capsule;
@@ -25,6 +28,7 @@ public class TimeCapsuleHubFragment extends DialogFragment {
     private View mRoot;
     private RecyclerView rvlist;
     private ArrayList<Capsule> capsules;
+    private Context mContext;
 
     @Override
     public void onAttach(Context context) {
@@ -37,11 +41,32 @@ public class TimeCapsuleHubFragment extends DialogFragment {
         mRoot = inflater.inflate(R.layout.fragment_time_capsulehub,container);
         rvlist = (RecyclerView) mRoot.findViewById(R.id.time_capsule_hub_recyclerView);
         rvlist.setAdapter(new HubAdapter(capsules, getActivity()));
-        rvlist.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvlist.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         return mRoot;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        // request a window without the title
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(true);
+        return dialog;
     }
 
     public void setCapsules(ArrayList<Capsule> capsules) {
         this.capsules = capsules;
+    }
+
+        @Override
+    public void onResume() {
+        super.onResume();
+        Window window = getDialog().getWindow();
+        if(window == null) return;
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.width = 1440;
+        params.height = 1550;
+        window.setAttributes(params);
     }
 }
